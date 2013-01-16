33c08dd041658a0c89e1bdcf8c9fac45f0f95b18
Northcode <northcode.no@gmail.com>
2013-01-16 17:28:21 +0100

    creation

---
+++ nvm/Extentions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace nvm
+{
+    public static class Extentions
+    {
+        public static byte[] ToBytes(this int e)
+        {
+            return BitConverter.GetBytes(e);
+        }
+
+        public static byte[] ToBytes(this float e)
+        {
+            return BitConverter.GetBytes(e);
+        }
+    }
+}
---
+++ nvm/Memory.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace nvm
+{
+    public class Buffer
+    {
+        // Different standard memory sizes
+        const int DEF_SIZE = 512;
+        const int KB = 1024;
+        const int MB = 1024 * 1000;
+        const int GB = 1024 * 1000 * 1000;
+
+        /// <summary>
+        /// For storing the data in the memory object
+        /// </summary>
+        byte[] data;
+
+        //Constuctors
+        public Buffer()
+        {
+            data = new byte[DEF_SIZE];
+        }
+
+        public Buffer(int size)
+        {
+            data = new byte[size];
+        }
+
+        //Write methods
+        public void Write(uint address, byte value)
+        {
+            data[address] = value;
+        }
+
+        public void Write(uint address, int value)
+        {
+            byte[] convertedValues = value.ToBytes();
+            data[address + 0] = convertedValues[0];
+            data[address + 1] = convertedValues[1];
+            data[address + 2] = convertedValues[2];
+            data[address + 3] = convertedValues[3];
+        }
+
+        public void Write(uint address, float value)
+        {
+            byte[] convertedValues = value.ToBytes();
+            data[address + 0] = convertedValues[0];
+            data[address + 1] = convertedValues[1];
+            data[address + 2] = convertedValues[2];
+            data[address + 3] = convertedValues[3];
+        }
+
+        public void Write(uint address, string value)
+        {
+            for (int i = 0; i < value.Length; i++)
+            {
+                Write((uint)(address + i), ((byte)value[i]));
+            }
+            Write((uint)value.Length + 1, 0x00);
+        }
+
+        public byte Read(uint address)
+        {
+            return data[address];
+        }
+
+        public int ReadInt(uint address)
+        {
+            return BitConverter.ToInt32(new byte[] { data[address], data[address + 1], data[address + 2], data[address + 3] },0);
+        }
+
+        public float ReadFloat(uint address)
+        {
+            return BitConverter.ToSingle(new byte[] { data[address], data[address + 1], data[address + 2], data[address + 3] }, 0);
+        }
+
+        public string ReadString(uint address)
+        {
+            StringBuilder sb = new StringBuilder();
+            int i = 0;
+            while (data[address + i] == 0x00)
+            {
+                sb.Append(data[address + i]);
+                i++;
+            }
+            return sb.ToString();
+        }
+    }
+}
